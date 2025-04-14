f72eae39ae44c32e723a25d6f05a4f08874a5e49
claudyadestine <cacayaju@gmail.com>
2025-04-14 12:13:48 +0700

    implementasi js5.2

--- POS/app/Models/LevelModel.php
+++ POS/app/Models/LevelModel.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class LevelModel extends Model
 {
-    protected $table = "m_level";        // Mendefinisikan nama tabel yang digunakan oleh model ini
-
-    protected $primaryKey = "level_id";      // Mendefinisikan primary key
+    protected $table = "m_level"; // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = "level_id"; // Mendefinisikan primary key
+    protected $fillable = ["level_kode", "level_nama"];
 }
