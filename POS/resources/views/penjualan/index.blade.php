09203476d032332fa753da2adf69b5c50fa0931d
claudyadestine <cacayaju@gmail.com>
2025-04-16 20:24:46 +0700

    implementasi js8

--- POS/resources/views/penjualan/index.blade.php
+++ POS/resources/views/penjualan/index.blade.php
@@ -5,10 +5,10 @@
         <div class="card-header">
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
-                <button onclick="modalAction('{{ url('/level/import') }}')" class="btn btn-info">Import Level</button>
-                <a href="{{ url('/level/export_excel') }}" class="btn btn-primary">Export Level</a>
-                <a href="{{ url('/level/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file- pdf"></i> Export Level (PDF)</a>
-                <button onclick="modalAction('{{ url('/level/create_ajax') }}')" class="btn btn-success">Tambah Data (Ajax)</button>
+                <button onclick="modalAction('{{ url('/detail_penjualan/import') }}')" class="btn btn-info">Import penjualan</button>
+                <a href="{{ url('/detail_penjualan/export_excel') }}" class="btn btn-primary">Export penjualan</a>
+                <a href="{{ url('/detail_penjualan/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file- pdf"></i> Export penjualan (PDF)</a>
+                <button onclick="modalAction('{{ url('/detail_penjualan/create_ajax') }}')" class="btn btn-success">Tambah Data (Ajax)</button>
             </div>
         </div>
         <div class="card-body">            
@@ -20,12 +20,15 @@
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
             
-            <table class="table table-bordered table-sm table-striped table-hover" id="table-level">
+            <table class="table table-bordered table-sm table-striped table-hover" id="table - detail_penjualan">
                 <thead>
                     <tr>
                         <th>No</th>
-                        <th>Kode Level</th>
-                        <th>Nama Level</th>
+                        <th>Detail Id</th>
+                        <th>Penjualan Id</th>
+                        <th>Barang Id</th>
+                        <th>Harga</th>
+                        <th>Jumlah </th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
@@ -44,13 +47,13 @@
                 $('#myModal').modal('show');
             });
         }
-        var tableLevel;
+        var tabledetail_penjualan;
         $(document).ready(function() {
-            tableLevel = $('#table-level').DataTable({
+            tabledetail_penjualan = $('#table-detail_penjualan').DataTable({
                 processing: true,
                 serverSide: true, 
                 ajax: {
-                    "url": "{{ url('level/list') }}",
+                    "url": "{{ url('detail_penjualan/list') }}",
                     "dataType": "json",
                     "type": "POST",
                 },
@@ -63,32 +66,53 @@
                         searchable: false
                     },
                     {
-                        data: "level_kode",
-                        className: "",
-                        width: "10%",
+                        data: "detail_id",
+                        className: "text-center",
+                        width: "6%",
                         orderable: true,
                         searchable: true
                     },
                     {
-                        data: "level_nama",
-                        className: "",
-                        width: "37%",
+                        data: "penjualan_id",
+                        className: "text-center",
+                        width: "6%",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "barang_id",
+                        className: "text-center",
+                        width: "6%",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "harga",
+                        className: "text-center",
+                        width: "6%",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "jumlah",
+                        className: "text-center",
+                        width: "10%",
                         orderable: true,
                         searchable: true
                     },
                     {
                         data: "aksi",
                         className: "text-center",
-                        width: "14%",
+                        width: "10%",
                         orderable: false,
                         searchable: false
                     }
                 ]
             });
 
-            $('#table-level_filter input').unbind().bind().on('keyup', function(e) {
+            $('#table-detail_penjualan_filter input').unbind().bind().on('keyup', function(e) {
                 if (e.keyCode == 13) { // enter key
-                    tableLevel.search(this.value).draw();
+                    tabledetail_penjualan.search(this.value).draw();
                 }
             });
         });
